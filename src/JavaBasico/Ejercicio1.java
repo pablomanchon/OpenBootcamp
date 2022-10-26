72cd92dc5c012e3df3eb2bd2bd0171d85b6e82ee
pablomanchon <pablomanchon97@gmail.com>
2022-10-26 15:46:19 -0300

    commit 1

---
+++ src/JavaBasico/Ejercicio1.java
@@ -0,0 +1,36 @@
+package JavaBasico;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int entero = 10;
+        String caracteres = "Hola mundo";
+        char caracter = 'j';
+        boolean logico = true;
+        byte num = 5;
+        long num2 = 6;
+        double dec = 1.5d;
+        float dec2 = 1.13f;
+        Integer i = null;
+        Float f = null;
+        Double d = null;
+        Byte b = null;
+        Long l = null;
+        Boolean log = null;
+
+
+        System.out.println("int: " + entero);
+        System.out.println("String: " + caracteres);
+        System.out.println("char: " + caracter);
+        System.out.println("boolean: " + logico);
+        System.out.println("byte: " + num);
+        System.out.println("double: " + dec);
+        System.out.println("long: " + num2);
+        System.out.println("float: " + dec2);
+        System.out.println("Integer: " + i);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Byte: " + b);
+        System.out.println("Long: " + l);
+        System.out.println("Boolean: " + log);
+    }
+}
